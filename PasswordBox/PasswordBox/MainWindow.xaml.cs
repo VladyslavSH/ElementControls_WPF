43a7facbd199668a2d90a269327c952e1b04a3da
VladyslavSH <manujlenkovlad2@gmail.com>
2018-06-18 17:53:59 +0300

    work is changed

--- PasswordBox/PasswordBox/MainWindow.xaml.cs
+++ PasswordBox/PasswordBox/MainWindow.xaml.cs
@@ -22,11 +22,13 @@ namespace PasswordBox
     {
         string mask;
         string buf = null;
+        int max = 0;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             buf = textBox.Text;
@@ -56,6 +58,28 @@ namespace PasswordBox
             {
                 passwordBox.PasswordChar = (mask = "*")[0];
             }
+        }
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            change_count.Content = passwordBox.Password.Length.ToString();
+        }
+
+        
+        private void MaxLengt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (MaxLengt.Text == String.Empty)
+            {
+                max = 0;
+                CurrentMax.Content = "Enter Max";
+            }
+            else { CurrentMax.Content = MaxLengt.Text; max = Convert.ToInt32(MaxLengt.Text); }
+            passwordBox.MaxLength = max;
+        }
+
+        private void MaxLengt_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0)) e.Handled = true;
         }
     }
 }
